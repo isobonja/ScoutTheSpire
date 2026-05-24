eb725538dcaec2d4d304af284923a7245d9f66f3
Ivan <isobonja@gmail.com>
2026-05-24 19:21:00 -0400

    Made plan for enemy info

--- app/src/components/EnemyEncounterInfoBox.tsx
+++ app/src/components/EnemyEncounterInfoBox.tsx
@@ -1,5 +1,6 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { capitalize } from "@/utils/general";
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 
 
 
@@ -7,6 +8,47 @@ function EnemyEncounterInfoBox() {
 
   const TABS = ["Encounters", "Enemies"]
 
+
+  /*
+    Enemies tab will include a table with rows containing 
+    enemy icon, name, act(s), times encountered, total times killed, 
+    total times died to.
+
+    Clicking on a row will show per-character stats on the right of the 
+    table for large window sizes. 
+
+    For smaller window sizes, the table will act as an accordian, 
+    where clicking on a row will expand a section below the row 
+    with the per-character stats.
+
+    -----
+
+    The data table columns will be Name, Act(s), Times Encountered, 
+      Times Killed, Times Died To.
+
+    The icon will be created from enemy images retrieved from Spire-Codex. 
+    Act info is located in the /api/enemies endpoint.
+    Times Encountered, Killed, and Died To can be calculated from the 
+    fight_stats array in the profile data.
+
+    Each data object for the table will look like this:
+    {
+      id: string
+      name: string
+      icon: string (url)
+      acts: number[]
+      timesEncountered: number
+      timesKilled: number
+      timesDiedTo: number
+      fightStats: {
+        character: string
+        losses: number
+        wins: number
+      }[]
+    }
+
+
+  */
   return (
     <Tabs defaultValue={TABS[0]} className="w-full h-160 pb-2 mb-2">
       <TabsList className='bg-transparent p-0 h-auto gap-2 mb-0 ps-4'>
@@ -36,25 +78,63 @@ function EnemyEncounterInfoBox() {
           </TabsTrigger>
         ))}
       </TabsList>
-      {Object.values(TABS).map((tab) => (
-        <TabsContent 
-          key={tab} 
-          value={tab} 
-          className={`
-            w-full
-            h-full 
-            info-panel-colorless
-            border-2!
-            border-slate-300
-            bg-transparent
-            rounded-md
-            mt-0!
-            relative
-          `}
-        >
-          <h1 className='text-4xl ms-4 mt-2 text-orange-300 font-extrabold font-heading tracking-wide'>{capitalize(tab)}</h1>
-        </TabsContent>
-      ))}
+
+      <TabsContent 
+        key="Encounters" 
+        value="Encounters" 
+        className={`
+          w-full
+          h-full 
+          info-panel-colorless
+          border-2!
+          border-slate-300
+          bg-transparent
+          rounded-md
+          mt-0!
+          relative
+        `}
+      >
+        <h1 className='text-4xl ms-4 mt-2 text-orange-300 font-extrabold font-heading tracking-wide'>{capitalize(TABS[0])}</h1>
+      </TabsContent>
+
+      <TabsContent 
+        key="Enemies" 
+        value="Enemies" 
+        className={`
+          w-full
+          h-full 
+          info-panel-colorless
+          border-2!
+          border-slate-300
+          bg-transparent
+          rounded-md
+          mt-0!
+          relative
+        `}
+      >
+        <h1 className='text-4xl ms-4 mt-2 text-orange-300 font-extrabold font-heading tracking-wide'>{capitalize(TABS[1])}</h1>
+        <Table>
+          <TableCaption>A list of your recent invoices.</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="">Invoice</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Method</TableHead>
+              <TableHead className="text-right">Amount</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell className="font-medium">INV001</TableCell>
+              <TableCell>Paid</TableCell>
+              <TableCell>Credit Card</TableCell>
+              <TableCell className="text-right">$250.00</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TabsContent>
+
+      
     </Tabs>
   )
 }
